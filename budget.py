9a8839d0a125205a7dd4a93f3fd0c2ba3f80fb77
Mateusz Środa <s99717@pollub.edu.pl>
2025-12-15 13:15:30 +0100

    Dodanie podstawowej klasy BudgetManager

--- budget.py
+++ budget.py
@@ -12,3 +12,15 @@ class Expense:
             raise ValueError("Kwota musi być dodatnia")
         if not self.title:
             raise ValueError("Tytuł nie może być pusty")
+        
+class BudgetManager:
+    def __init__(self, balance=0):
+        self.balance = balance
+        self.expenses = []
+
+    def add_expense(self, title, amount):
+        expense = Expense(title, amount)
+        if amount > self.balance:
+            raise ValueError("Insufficient funds")
+        self.expenses.append(expense)
+        self.balance -= amount
