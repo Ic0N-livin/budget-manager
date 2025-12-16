2fd35751a81fffaefc3035297529e899789fc496
Mateusz Środa <mateuszsroda049@gmail.com>
2025-12-16 14:21:29 +0100

    Dodanie dwóch testów

--- tests.py
+++ tests.py
@@ -7,12 +7,10 @@ class TestBudgetApp(unittest.TestCase):
         self.assertEqual(exp.amount, 12.0)
 
     def test_expense_validation(self):
-        # Test: sprawdź, czy ujemna kwota powoduje błąd
         with self.assertRaises(ValueError):
             Expense("Error", -5)
 
     def test_manager_integration(self):
-        # Test integracyjny: Menedżer + Koszty
         SALDO_STARTOWE = 100
         manager = BudgetManager(SALDO_STARTOWE)
         manager.add_expense("Jedzenie", 30)
@@ -24,6 +22,18 @@ class TestBudgetApp(unittest.TestCase):
         manager = BudgetManager(SALDO_STARTOWE)
         with self.assertRaises(ValueError):
             manager.add_expense("Zegarek", 100)
+
+    def test_multiple_expanses(self):
+        SALDO_STARTOWE = 150
+        manager = BudgetManager(SALDO_STARTOWE)
+        manager.add_expense("Jedzenie", 30)
+        manager.add_expense("Prezenty", 70)
+        self.assertEqual(len(manager.expenses), 2)
+
+    def test_free_expanse(self):
+        with self.assertRaises(ValueError):
+            Expense("Darmowa rzecz", 0)
+
 
 if __name__ == '__main__':
     unittest.main()
